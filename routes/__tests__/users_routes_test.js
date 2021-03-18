3d92e80441927257f5be680fc2b3fe4e8ce30e0f
Eric Choi <eric12397@gmail.com>
2021-03-17 22:57:59 -0400

    added tests for user authentication routes

--- routes/__tests__/users_routes_test.js
+++ routes/__tests__/users_routes_test.js
@@ -7,6 +7,17 @@ const User = require('../../models/user/user_model');
 
 let server;
 
+const getToken = async () => {
+  const response = await request(server)
+    .post('/api/auth/login')
+    .send({ username: 'Gordi', password: 'IAmGordi' })
+  
+  console.log(response.body)  
+  let token = response.body.token;
+
+  return token;
+}
+
 const createSeedData = async () => {
   const password = "IAmGordi";
   const salt = await bcrypt.genSalt(10);
@@ -43,17 +54,91 @@ describe('Testing routes', () => {
   })
 
 
-  it ("should GET all users", async () => {
-    await request(server)
+  it ("should get all users", async () => {
+    const response = await request(server)
       .get('/api/users')
       .expect(200)
-      .then(response => {
-        expect(response.body.length).toBe(1);
-        expect(response.body[0]).toHaveProperty("username");
-        expect(response.body[0]).not.toHaveProperty("password");
-      })
+      
+    expect(response.body.length).toBe(1);
+    expect(response.body[0]).toHaveProperty("username");
+    expect(response.body[0]).not.toHaveProperty("password");
+      
   })
 
+
+  it ("should register a new user and get back a valid token", async () => {
+    const response = await request(server)
+      .post('/api/auth/register')
+      .send({ username: "Shaq", password: "IAmShaq" })
+      .expect(201)
+
+    const createdUser = response.body.user;
+    const token = response.body.token;
+      
+    expect(token).toBeDefined();
+    expect(createdUser.id).toBeDefined();
+    expect(createdUser.username).toBe("Shaq");
+  })
+
+
+  it ("should fail to register an existing user", async () => {
+    const response = await request(server)
+      .post('/api/auth/register')
+      .send({ username: "Gordi", password: "IAmGordi" })
+      .expect(400)
+  })
+
+
+  it ("should login successfully and get back a valid token", async () => {
+    const response = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'Gordi', password: 'IAmGordi' })
+      .expect(200);
+  
+    let token = response.body.token;
+    expect(token).toBeDefined();
+  })
+
+
+  it ("should fail to login with wrong credentials", async () => {
+    const response = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'Gordi', password: 'wrong password' })
+      .expect(400);
+  })
+
+
+  it ("should fail to login a user who is not registered yet", async () => {
+    const response = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'NotGordi', password: 'IAmNotGordi' })
+      .expect(400);
+  })
+
+
+  it ("should get a protected user with auth", async () => {
+    const token = getToken();
+    const user = User.findOne({ username: 'Gordi' });
+
+    const response = await request(server)
+      .get('/api/auth/login')
+      .set('Accept', 'application/json')
+      .set('Authorization', token)
+      .expect(200);
+
+    console.log(response)
+    const { id, username } = response.body
+
+    expect(user._id).toEqual(id);
+    expect(user.username).toEqual(username);
+  })
+
+
+  it ("should fail to get a protected user without auth", async () => {
+    await request(server)
+      .get('/api/auth/user')
+      .expect(401)
+  })
 })
 
 
